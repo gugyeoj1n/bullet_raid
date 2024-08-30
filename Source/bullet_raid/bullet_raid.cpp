171df176c584ae99d91b96ced9d14eeb75f73bbc
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2024-08-30 21:18:22 +0900

    [CHORE] 프로젝트 생성 및 에셋 추가

---
+++ Source/bullet_raid/bullet_raid.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "bullet_raid.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, bullet_raid, "bullet_raid" );
